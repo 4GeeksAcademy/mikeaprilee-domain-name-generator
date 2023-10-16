96c500c279a2e8fc4bd373a378272fc0ff86b7ce
Michael Aprile Mancha <130480625+mikeaprilee@users.noreply.github.com>
2023-10-16 18:59:17 +0000

    sin prettier

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  
+}
+console.log("Hello Rigo from the console!");
 };
